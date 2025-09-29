c753b4769f15d75bfc5659b197069d687fba4354
Vatsal <vpgajjar012@gmail.com>
2025-09-29 18:08:23 +0530

    Configure telit after firmware update

--- telit-firmware-508-update-scripts/update-firmware.sh
+++ telit-firmware-508-update-scripts/update-firmware.sh
@@ -174,6 +174,44 @@ update_firmware_via_uart() {
     fi
 }
 
+configure_telit() {
+    oizom-config --gsmport=$(get_gsm_port) --modemcommand="AT#FWSWITCH=40,1,0"
+    wait_for_gsm_port || exit 1
+    fwswitch_status=$(oizom-config --gsmport=$(get_gsm_port) --modemcommand="AT#FWSWITCH?" | grep -c 'FWSWITCH: 40,0,0')
+    if [ "$fwswitch_status" -eq 0 ]; then
+        echo "Failed to set FWSWITCH. Exiting."
+        exit 1
+    fi
+
+    echo "Clearing APN settings..."
+    wait_for_gsm_port || exit 1
+    oizom-config --gsmport=$(get_gsm_port) --modemcommand="AT+CGDCONT=1,\"IPV4V6\",\"\""
+    sleep 5
+    cgdcont_status=$(oizom-config --gsmport=$(get_gsm_port) --modemcommand="AT+CGDCONT?" | grep -c 'CGDCONT: 1,"IPV4V6","","",0,0,0,0')
+    if [ "$cgdcont_status" -eq 0 ]; then
+        echo "Failed to clear APN settings. Exiting."
+        exit 1
+    fi
+
+    echo "Setting USBCFG..."
+    wait_for_gsm_port || exit 1
+    oizom-config --gsmport=$(get_gsm_port) --modemcommand="AT#USBCFG=1"
+    wait_for_gsm_port || exit 1
+    usbcfg_status=$(oizom-config --gsmport=$(get_gsm_port) --modemcommand="AT#USBCFG?" | grep -c 'USBCFG: 1')
+    if [ "$usbcfg_status" -eq 0 ]; then
+        echo "Failed to set USBCFG. Exiting."
+    fi
+
+    echo "Setting ECM..."
+    wait_for_gsm_port || exit 1
+    oizom-config --gsmport=$(get_gsm_port) --modemcommand="AT#ECM=1,0"
+    sleep 5
+    ecm_status=$(oizom-config --gsmport=$(get_gsm_port) --modemcommand="AT#ECM?" | grep -c 'ECM: 0,1')
+    if [ "$ecm_status" -eq 0 ]; then
+        echo "Failed to set ECM. Exiting."
+    fi
+}
+
 pre_script_tasks
 identify_module
 download_files
@@ -182,6 +220,7 @@ if module_bootmode_status; then
     reboot_module
 fi
 update_firmware_via_uart
+configure_telit
 post_script_tasks
 
 echo "Update successful. Please update debian package."
